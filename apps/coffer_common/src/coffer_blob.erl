35f4cff36f0300df18838ab78c029f69921feee5
benoitc <bchesneau@gmail.com>
2013-11-10 22:44:50 +0100

    add a common module to parse a blob ref

---
+++ apps/coffer_common/src/coffer_blob.erl
@@ -0,0 +1,35 @@
+-module(coffer_blob).
+
+-export([blob_regexp/0]).
+-export([parse_blobref/1]).
+-export([validate_blobref/1]).
+
+parse_blobref(BlobRef) ->
+    Re = blob_regexp(),
+    case re:run(BlobRef, Re, [{capture, all, binary}]) of
+        nomatch ->
+            error;
+        Matches ->
+            {match, [_, HashType, Hash]} = Matches,
+            {HashType, Hash}
+    end.
+
+validate_blobref(BlobRef) ->
+    Re = blob_regexp(),
+    case re:run(BlobRef, Re, [{capture, none}]) of
+        nomatch ->
+            error;
+        _ ->
+            ok
+    end.
+
+blob_regexp() ->
+    %% we cache the regexp so it can be reused in the same process
+    case get(blob_regexp) of
+        undefined ->
+            {ok, RegExp} = re:compile("^([a-z][a-zA-Z0-9^-]*)-([a-zA-Z0-9]*)$"),
+            put(blob_regexp, RegExp),
+            RegExp;
+        RegExp ->
+            RegExp
+    end.
